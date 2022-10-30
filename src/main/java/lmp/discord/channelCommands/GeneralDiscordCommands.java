67df30e836309d89fbc4c9dabb488aeadadd11aa
latch93 <cody.latiolais@gmail.com>
2022-10-29 19:02:08 -0500

    Should have fixed NPE when new member joins discord

--- src/main/java/lmp/discord/channelCommands/GeneralDiscordCommands.java
+++ src/main/java/lmp/discord/channelCommands/GeneralDiscordCommands.java
@@ -1,5 +1,6 @@
 package lmp.discord.channelCommands;
 
+import lmp.Constants;
 import lmp.LatchDiscord;
 import lmp.Main;
 import net.dv8tion.jda.api.JDA;
@@ -27,26 +28,27 @@ public class GeneralDiscordCommands extends ListenerAdapter {
         MessageChannel channel = e.getChannel();
         JDA jda = LatchDiscord.getJDA();
         Member discordMember = e.getMember();
-        assert discordMember != null;
-        if (messageContent.equalsIgnoreCase("!discordID")) {
-            channel.sendMessage("Hi " + discordUserName + "! Your Discord UserID is " + discordUserID).queue();
-        }
-        if (!discordMember.getRoles().toString().contains("Member")) {
-            channel.sendMessage("Type !link in this channel to get your link command and the Server IP dm'd to you by my bot.").queue();
-            Objects.requireNonNull(jda.getGuildById(lmp.Constants.GUILD_ID)).addRoleToMember(UserSnowflake.fromId(discordUserID), Objects.requireNonNull(jda.getRoleById(lmp.Constants.MEMBER_ROLE_ID))).queue();
-        }
-        if (messageContent.equalsIgnoreCase("!joinTime")) {
-            channel.sendMessage(discordUserName + " joined on " + discordMember.getTimeJoined().toString().split("T")[0]).queue();
-        }
-        if (messageContent.equalsIgnoreCase("!link")) {
-            discordUser.openPrivateChannel().flatMap(dm -> dm.sendMessage("IP = latch.ddns.net\n" +
-                    "Java Port Number = 60\n" +
-                    "Bedrock Port Number = 19132\n" +
-                    "Run the following command in your minecraft client chat after you join:\n" +
-                    "/lmp link " + discordUserID)).queue(null, new ErrorHandler()
-                    .handle(ErrorResponse.CANNOT_SEND_TO_USER,
-                            (ex) -> Main.log.warning("Cannot send link message to " + discordUserName)));
-            channel.sendMessage(discordUserName + " --- Check your Discord for a private message from my bot containing your link command. <:LatchPOG:957363669388386404>").queue();
+        if (discordMember != null && !discordUserID.equalsIgnoreCase(Constants.LATCH93BOT_USER_ID)) {
+            if (messageContent.equalsIgnoreCase("!discordID")) {
+                channel.sendMessage("Hi " + discordUserName + "! Your Discord UserID is " + discordUserID).queue();
+            }
+            if (!discordMember.getRoles().toString().contains("Member")) {
+                channel.sendMessage("Type !link in this channel to get your link command and the Server IP dm'd to you by my bot.").queue();
+                Objects.requireNonNull(jda.getGuildById(lmp.Constants.GUILD_ID)).addRoleToMember(UserSnowflake.fromId(discordUserID), Objects.requireNonNull(jda.getRoleById(lmp.Constants.MEMBER_ROLE_ID))).queue();
+            }
+            if (messageContent.equalsIgnoreCase("!joinTime")) {
+                channel.sendMessage(discordUserName + " joined on " + discordMember.getTimeJoined().toString().split("T")[0]).queue();
+            }
+            if (messageContent.equalsIgnoreCase("!link")) {
+                discordUser.openPrivateChannel().flatMap(dm -> dm.sendMessage("IP = latch.ddns.net\n" +
+                        "Java Port Number = 60\n" +
+                        "Bedrock Port Number = 19132\n" +
+                        "Run the following command in your minecraft client chat after you join:\n" +
+                        "/lmp link " + discordUserID)).queue(null, new ErrorHandler()
+                        .handle(ErrorResponse.CANNOT_SEND_TO_USER,
+                                (ex) -> Main.log.warning("Cannot send link message to " + discordUserName)));
+                channel.sendMessage(discordUserName + " --- Check your Discord for a private message from my bot containing your link command. <:LatchPOG:957363669388386404>").queue();
+            }
         }
     }
 }
